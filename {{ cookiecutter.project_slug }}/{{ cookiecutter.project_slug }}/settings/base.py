5947df7c2bc287ea7db6b663836193484d34952a
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-06-16 08:15:48 +0200

    Move .env.example to project root
    I've decided to move .env.example to project root and so I had to read
    environment file within manage.py files.

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings/base.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings/base.py
@@ -14,7 +14,6 @@ import environ
 import os
 
 env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))

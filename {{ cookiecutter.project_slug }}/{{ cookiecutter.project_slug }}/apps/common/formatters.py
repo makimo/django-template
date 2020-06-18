838d85b2333948818ee017f546d242161ee2db0d
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-06-18 09:51:13 +0200

    Do not change log format when content type of request is not application/json
    Rationale of this change is to block oversized POST bodies like file
    attachments.

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/apps/common/formatters.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/apps/common/formatters.py
@@ -2,9 +2,20 @@ from pythonjsonlogger import jsonlogger
 
 
 class DjangoRequestJsonFormatter(jsonlogger.JsonFormatter):
+    """
+    This class is used to serialize headers and body of the JSON Django request
+    into the `request` property in the record of `jsonlogger.JsonFormatter`.
+    """
+
     def add_fields(self, log_record, record, message_dict):
         if getattr(record, 'request', None):
-            if getattr(record.request, 'method', None):
+            def is_django_request(request):
+                return getattr(record.request, 'method', None)
+
+            def is_json_request(request):
+                return record.request.META.get('CONTENT_TYPE', None) == 'application/json'
+
+            if is_django_request(record.request) and is_json_request(record.request):
                 record.request = {
                     'request': record.request,
                     'headers': record.request.headers,
